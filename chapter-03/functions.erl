8ed45dbe14f5520340600ea0f4a42db91c04f53a
Carlos Valiente <carlos@pepelabs.net>
2015-07-07 21:32:04 +0100

    More from chapter 3

--- chapter-03/functions.erl
+++ chapter-03/functions.erl
@@ -1,5 +1,5 @@
 -module(functions).
--export([head/1, old_enough/1, same/2, second/1]).
+-export([head/1, old_enough/1, right_age/1, same/2, second/1]).
 
 head([X|_]) -> X.
 
@@ -13,4 +13,8 @@ same(_, _) ->
 old_enough(X) when X >= 16 ->
     true;
 old_enough(_) -> false.
+
+right_age(X) when X >= 16, X =< 104 ->
+    true;
+right_age(_) -> false.
 
